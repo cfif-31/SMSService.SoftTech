3d15a24f9f388e692a9067fd4a8a6086aab4b912
cfif31 <i.cfif31@gmail.com>
2023-02-04 16:03:47 +0400

    registration service in DI

---
+++ SMSService.SoftTech.Application/Extentions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SMSService.SoftTech.Application.Services.DataServices;
+using SMSService.SoftTech.Application.Services.DataServices.Interfaces;
+using SMSService.SoftTech.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSService.SoftTech.Application.Extentions
+{
+    public static class ApplicationServiceExtensions
+    {
+        /// <summary>
+        /// Registration Application module in DI
+        /// </summary>
+        /// <param name="services">Service collections</param>
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            //Data services
+            services.AddScoped<ISmsMessageService, SmsMessageService>();
+            services.AddScoped<ISmsStateService, SmsStateService>();
+        }
+    }
+}
